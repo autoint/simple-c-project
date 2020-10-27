3a1ccb1d0e941e1c706f1417ad87711971587d35
Niroshan Rajadurai <niroshan@github.com>
2020-10-27 10:51:42 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
+// Program to calculate the sum and average of positive numbers
+// If the user enters a negative number, the sum and average are displayed.
+
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World\n");
+int main() {
+
+   const int maxInput = 100;
+   int i;
+   double number, average, sum = 0.0;
     
-    return 0;
+   printf("Program to calculate the sum and average of positive numbers\n");
+   printf("If the user enters a negative number, the sum and average are displayed.\n");
+
+   for (i = 1; i <= maxInput; ++i) {
+      printf("%d. Enter a number: ", i);
+      scanf("%lf", &number);
+      
+      // go to jump if the user enters a negative number
+      if (number < 0.0) {
+         goto jump;
+      }
+      sum += number;
+   }
+
+jump:
+   average = sum / (i - 1);
+   printf("Sum = %.2f\n", sum);
+   printf("Average = %.2f", average);
+
+   return 0;
 }
